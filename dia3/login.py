369e18c64686dcb9305d3fedf2a968489105e7d5
Arthur Cunha Nonato <contato.arthurnonato@gmail.com>
2026-03-21 22:05:39 -0300

    finalizei o dia 3

--- dia3/login.py
+++ dia3/login.py
@@ -1,1 +1,10 @@
-print ('maionese')
+login = ''
+senha = ''
+print ('ok')
+login = input ('login: ').lower().strip()
+senha = input ('senha: ').strip()
+
+if login == 'arthur' and senha == '1234':
+    print ('seja bem vindo')
+else:
+    print ('acesso negado')
